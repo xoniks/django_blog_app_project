40f6b4ad188da1138192f44041ba8e74ea6f4680
e.baruti <63745689+xoniks@users.noreply.github.com>
2023-07-08 22:27:52 +0200

    done

--- django_blog_app_project/django_blog_app_project/settings.py
+++ django_blog_app_project/django_blog_app_project/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -113,3 +114,6 @@ STATIC_DIRS = [BASE_DIR/'static']
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
